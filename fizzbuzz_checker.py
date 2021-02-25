1ffe13757b32615e7b301b4162ba0cc4c8ea3738
Kylian Dorne <47109882+KyDrn@users.noreply.github.com>
2021-02-25 17:01:47 +0100

    Refactorisation code prod test multiple 3 et 5

--- fizzbuzz_checker.py
+++ fizzbuzz_checker.py
@@ -4,7 +4,7 @@ class FizzBuzzChecker:
     def is_fizzbuzz(number):
         if number <= 0:
             raise ValueError
-        if number == 15:
+        if number % 3 == 0 and number % 5 == 0:
             return "FizzBuzz"
         if number % 3 == 0:
             return "Fizz"
