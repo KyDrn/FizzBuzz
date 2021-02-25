573ead787a2434a72be4bce0a748ac4dc8c9db70
Kylian Dorne <47109882+KyDrn@users.noreply.github.com>
2021-02-25 16:59:52 +0100

    Init code prod test multiple 3 et 5

--- fizzbuzz_checker.py
+++ fizzbuzz_checker.py
@@ -4,8 +4,10 @@ class FizzBuzzChecker:
     def is_fizzbuzz(number):
         if number <= 0:
             raise ValueError
+        if number == 15:
+            return "FizzBuzz"
         if number % 3 == 0:
             return "Fizz"
-        else:
+        if number == 10:
             return "Buzz"
             
